d59a02bded38cd19a2ff9dbc1140b9510517c468
Barış Sokat <barissokat@outlook.com>
2022-01-12 12:06:43 +0300

    Send POST request

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App () {
     setError(null)
 
     try {
-      const response = await fetch('https://swapi.dev/api/films/')
+      const response = await fetch('https://maxi-react-authentication-default-rtdb.europe-west1.firebasedatabase.app/movies.json')
 
       if (!response.ok) {
         throw new Error('Something went wrong!')
@@ -23,16 +23,18 @@ function App () {
 
       const data = await response.json()
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
+      const loadedMovies = []
 
-      setMovies(transformedMovies)
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      setMovies(loadedMovies)
     } catch (error) {
       setError(error.message)
     }
@@ -43,8 +45,16 @@ function App () {
     fetchMoviesHandler()
   }, [fetchMoviesHandler])
 
-  const addMovieHandler = movie => {
-    console.log(movie)
+  async function addMovieHandler (movie) {
+    const response = await fetch('https://maxi-react-authentication-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log(data)
   }
 
   let content = <p>Found no movies.</p>
@@ -58,7 +68,7 @@ function App () {
   }
 
   if (isLoading) {
-    content = <div className='loading-box'><div className='loading' /></div>
+    content = <p>Loading...</p>
   }
 
   return (
