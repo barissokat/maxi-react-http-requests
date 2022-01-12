fbaf779c491066576d087e33e58e0cb261fe4883
Barış Sokat <barissokat@outlook.com>
2022-01-12 10:57:12 +0300

    Handle HTTP Errors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
+import { useCallback, useEffect, useState } from 'react'
 import MoviesList from './components/MoviesList'
+
 import './App.css'
-import { useState } from 'react'
 
 function App () {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  async function fetchMoviesHandler () {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json()
+    setError(null)
 
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
       }
-    })
 
-    setMovies(transformedMovies)
+      const data = await response.json()
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+
+      setMovies(transformedMovies)
+    } catch (error) {
+      setError(error.message)
+    }
     setIsLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
+
+  let content = <p>Found no movies.</p>
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <div className='loading-box'><div className='loading' /></div>
   }
 
   return (
@@ -29,13 +60,7 @@ function App () {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        <div className='loading-box'>
-          {isLoading && <div className='loading' />}
-        </div>
-      </section>
+      <section>{content}</section>
     </>
   )
 }
